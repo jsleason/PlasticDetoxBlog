e3f7de3363425b4425b95c510645256cbd52aeee
jsleason <jsleason@umich.edu>
2018-07-31 14:17:51 +0200

    progress

--- src/Blog.js
+++ src/Blog.js
@@ -1,37 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Paper from 'material-ui/Paper';
-//import Grid from 'material-ui/core/Grid';
+import { BlogPost } from './post';
+import { NewPost } from './post1';
 
-// const styles = theme => ({
-//     root: {
-//         flexGrow: 1,
-//     },
-//     paper: {
-//         padding: theme.spacing.unit * 2,
-//         textAlign: 'center',
-//         color: theme.palette.text.secondary,
-//     },
-// });
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+});
 
 class Blog extends React.Component {
     state = {
-        blogRef: "",
+        blogRef: ""
     }
+
+    showPost = (postRef) => {
+        this.setState({ blogRef: "postRef" });
+        const tesNode = ReactDOM.findDOMNode(this.refs.blogContent);
+        window.scrollTo(0, tesNode.offsetTop)
+    }
+
     render() {
-       const { classes } = this.props;
+        const { classes } = this.props;
         return (
-            <div id="blog">
-                <h1 className="headingsWhite">Blog</h1>
+            <div id="blog" className="BlogViewContainerStyle">
+                <Grid container spacing={24}>
+                    <Grid item xs={12} sm={12}>
+                        <h1 className='headingsWhite'>Blog</h1>
+                    </Grid>
+                    <Grid item xs={6} sm={4} >
+                        <div className={"blogSelection"} onClick="showPost(BlogPost)">
+                            <Paper >
+                                <h3>This is a post </h3>
+                            </Paper>
+                        </div>
+                    </Grid>
+                    <Grid item xs={6} sm={4} >
+                        <div className={"blogSelection"} onClick="showPost(NewPost)">
+                            <Paper >
+                                <h3>This is another post </h3>
+                            </Paper>
+                        </div>
+                    </Grid>
+                </Grid>
+
+
+                 <div class="blogContainerWindow" ref="blogContent">
+                    {
+                        // this.state.blogRef == "BlogPost" ?
+
+                        //     <BlogPost />
+
+                        //     :
+
+                        //     this.state.blogRef == "NewPost" ?
+
+                        //         <NewPost />
+                        //     :
+                        //         null
+				}
+                </div>
+
+
+
             </div>
         )
     }
 }
 
 Blog.propTypes = {
-	classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
 export default Blog;
---
+++ src/post.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+});
+
+class BlogPost extends React.Component {
+    render(){
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.root} id="blog" style={{textAlign: 'left'}}>
+                <Grid container spacing={24}>  
+                        <Grid item xs={12} sm={12}>
+                            <h1 className='headings' style={{color:'white'}}>Blog Post</h1>
+                            <hr className='headingUnderLine'/>
+                        </Grid>  
+                        <Grid item xs={12} sm={12}>
+
+                            <h3 style={{textAlign:'left'}}>This is post content.</h3>
+
+                        </Grid>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+BlogPost.propTypes = {
+	classes: PropTypes.object.isRequired
+};
+
+export default BlogPost;
